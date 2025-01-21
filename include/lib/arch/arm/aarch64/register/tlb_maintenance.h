17ab7e1d7ceca590db39dba45acc760da6f1dc6a
JunKi, Hong <dylanhong920509@gmail.com>
2025-01-21 22:56:05 +0900

    list-up aarch64 system register group, ref. DDI0500E_cortex_a53_r0p3_trm.pdf

--- include/lib/arch/arm/aarch64/register/tlb_maintenance.h
+++ include/lib/arch/arm/aarch64/register/tlb_maintenance.h
@@ -1,7 +1,81 @@
-/*******************************************************************************
+/****************************************************************************************************
  * AArch64 TLB maintenance operations
- *******************************************************************************/
+ ****************************************************************************************************/
 #ifndef TLB_MAINTENANCE__H__
 #define TLB_MAINTENANCE__H__
+
+/****************************************************************************************************
+ * TLBI VMALLE1IS:
+ *  Invalidate all stage 1 translations used at EL1 
+ *  with the current virtual machine identifier (VMID) 
+ *  in the Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAE1IS:
+ *  Invalidate translation used at EL1 
+ *  for the specified VA and Address Space Identifier (ASID) and the current VMID,
+ *  Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI ASIDE1IS:
+ *  Invalidate all translations used at EL1 with the current VMID and the supplied ASID, 
+ *  Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAAE1IS:
+ *  Invalidate all translations used at EL1 
+ *  for the specified address and current VMID and for all ASID values, 
+ *  Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VALE1IS:
+ *  Invalidate all entries from the last level of stage 1 translation table walk used at EL1 
+ *  with the supplied ASID and current VMID, 
+ *  Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAALE1IS:
+ *  Invalidate all entries from the last level of stage 1 translation table walk used at EL1 
+ *  for the specified address and current VMID and for all ASID values, 
+ *  Inner Shareable
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VMALLE1:
+ *  Invalidate all stage 1 translations used at EL1 with the current VMID
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAE1:
+ *  Invalidate translation used at EL1 for the specified VA and ASID and the current VMID
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI ASIDE1:
+ *  Invalidate all translations used at EL1 with the current VMID and the supplied ASID
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAAE1:
+ *  Invalidate all translations used at EL1 
+ *  for the specified address and current VMID and for all ASID values
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VALE1:
+ *  Invalidate all entries from the last level of stage 1 translation table walk used at EL1 
+ *  with the supplied ASID and current VMID
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * TLBI VAALE1:
+ *  Invalidate all entries from the last level of stage 1 translation table walk used at EL1 
+ *  for the specified address and current VMID and for all ASID values
+ ****************************************************************************************************/
 
 #endif  /* !TLB_MAINTENANCE__H__ */
